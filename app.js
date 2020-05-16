62491e799f0b3a65cc4a0997c605c3dd9ed8793e
mohammad-alfatih <alfaitha@gmail.com>
2020-05-16 09:58:12 -0400

    clean up app.js

--- app.js
+++ app.js
@@ -1,44 +1,37 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
 
-var homeRouter = require('./routes/home');
-var usersRouter = require('./routes/users');
+const cookieParser = require('cookie-parser');
 
-var app = express();
-var port = 3000;
+const homeRouter = require('./routes/home');
+const productsRouter = require('./routes/products');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', homeRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use('/products', productsRouter);
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  console.log(res.locals.message);
+  console.log(err.message);
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-app.listen(port, () => console.log('yadda yadda yadda'));
-
-// module.exports = app;
+app.listen(PORT, () => console.log(`yadda yadda yadda ${PORT}`));
