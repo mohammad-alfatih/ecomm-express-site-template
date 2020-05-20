7a2b95d9ceff210ac99508935fedb129dcb82e41
mohammad-alfatih <alfaitha@gmail.com>
2020-05-20 08:46:34 -0400

    render products view

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const headerLinks = require('../mockData/header-links');
+const footerLinks = require('../mockData/footer-links');
+const products = require('../mockData/product-list');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('products', { title: 'Products' });
+  const category = req.query.category
+
+  res.render('products', {
+    title: 'Our latest products',
+    headerLinks: headerLinks,
+    footerLinks: footerLinks
+  });
 });
 
 module.exports = router;
